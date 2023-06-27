408869869729d13931576e3fcaa2bb021d10f304
LeonPS <leonps531@gmail.com>
2023-06-27 15:21:46 +0100

    Add interface for project ir repo

--- modules/ir_repository/src/ir_repository/project/ProjectIrRepository.cpp
+++ modules/ir_repository/src/ir_repository/project/ProjectIrRepository.cpp
@@ -1,1 +1,34 @@
 #include "ProjectIrRepository.h"
+ProjectIrRepository::ProjectIrRepository (lager::reader<ProjectIrRepositoryModel> reader,
+                                          lager::context<ProjectIrRepositoryAction> context)
+{
+}
+
+void ProjectIrRepository::LinkProjectPath (const std::filesystem::path & project_path)
+{
+}
+
+std::filesystem::path ProjectIrRepository::GetProjectPath ()
+{
+    return std::filesystem::path ();
+}
+
+void ProjectIrRepository::LoadNewProjectIr (const IrMetadata & ir_metadata,
+                                            const IrData & ir_data,
+                                            ProjectIrRepository::LoadNewProjectIrCallback callback)
+{
+}
+
+IrMetadata ProjectIrRepository::LoadIrMetaData (const std::string & ir_identifier)
+{
+    return IrMetadata ();
+}
+
+void ProjectIrRepository::LoadIrData (const std::string & ir_identifier, IrData & ir_data)
+{
+}
+
+bool ProjectIrRepository::IsPathValidDirectory (const std::filesystem::path & path)
+{
+    return false;
+}
--- modules/ir_repository/src/ir_repository/project/ProjectIrRepository.h
+++ modules/ir_repository/src/ir_repository/project/ProjectIrRepository.h
@@ -7,17 +7,24 @@
 #include "ProjectIrRepositoryModel.h"
 
 #include <filesystem>
+#include <functional>
 #include <lager/context.hpp>
 #include <lager/reader.hpp>
 
 class ProjectIrRepository
 {
 public:
+    using LoadNewProjectIrCallback = std::function<void (const std::string &)>;
+
     ProjectIrRepository (lager::reader<ProjectIrRepositoryModel> reader,
                          lager::context<ProjectIrRepositoryAction> context);
 
     void LinkProjectPath (const std::filesystem::path & project_path);
     std::filesystem::path GetProjectPath ();
+
+    void LoadNewProjectIr (const IrMetadata & ir_metadata,
+                           const IrData & ir_data,
+                           LoadNewProjectIrCallback callback);
 
     [[nodiscard]] IrMetadata LoadIrMetaData (const std::string & ir_identifier);
     void LoadIrData (const std::string & ir_identifier, IrData & ir_data);
