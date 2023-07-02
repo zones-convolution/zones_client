d0af864cf7d3b23289325bb2fee08092d8deba50
Micah Strange <micasans@btinternet.com>
2023-07-02 15:55:54 +0100

    hashing

--- modules/ir_engine/src/ir_engine/IrGraphHashing.h
+++ modules/ir_engine/src/ir_engine/IrGraphHashing.h
@@ -6,12 +6,11 @@
 class IrGraphHashing
 {
 public:
-    [[nodiscard]] static std::size_t CombineProcessorHashes (size_t previous_processor_hash,
-                                                             size_t next_processor_hash);
-
     [[nodiscard]] static std::size_t GetHashForCachePolicy (const IrGraphCachePolicy & cache_policy,
                                                             size_t previous_processor_hash,
                                                             const IrGraphState & ir_graph_state);
 
 private:
+    [[nodiscard]] static std::size_t CombineProcessorHashes (size_t previous_processor_hash,
+                                                             size_t next_processor_hash);
 };
---
+++ modules/ir_engine/test/IrGraphHashingTests.cpp
@@ -0,0 +1,5 @@
+#include "ir_engine/IrGraphHashing.h"
+
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE ("")
