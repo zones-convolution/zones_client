c23f66e5d92150e334d222129977c8e6e56bae2a
Micah Strange <micasans@btinternet.com>
2023-07-31 15:33:52 +0100

    testing tabs

--- modules/layout/test/TabsActionTests.cpp
+++ modules/layout/test/TabsActionTests.cpp
@@ -1,18 +1,40 @@
 #include "layout/tabs/TabsAction.h"
 
 #include <catch2/catch_test_macros.hpp>
+#include <future>
+#include <lager/context.hpp>
+#include <lager/event_loop/manual.hpp>
+#include <lager/store.hpp>
+
+// lager::detail::dispatcher<lager::as_actions_t<TabsAction>> dispatcher {
+//    [] (auto && act) { return lager::promise::invalid (); }};
+
+struct TabsControllerDelegateMock : public TabsControllerDelegate
+{
+    bool LoadTab (const std::string & tab_name, bool animate) override
+    {
+        return false;
+    }
+};
 
 SCENARIO ("updating tabs model", "[TabsAction]")
 {
     GIVEN ("a tabs model")
     {
+        TabsControllerDelegateMock tabs_controller_delegate_mock;
+        auto store = lager::make_store<TabsAction> (
+            TabsModel {},
+            lager::with_manual_event_loop {},
+            lager::with_reducer (UpdateTabs),
+            lager::with_deps (std::ref (tabs_controller_delegate_mock)));
+
         WHEN ("a load tab action is dispatched")
         {
             LoadTabAction load_tab_action {.tab_name = "test_tab"};
-            auto [model, effect] = UpdateTabs (TabsModel {}, load_tab_action);
+            store.dispatch (load_tab_action);
             THEN ("the current tab has not changed")
             {
-                REQUIRE (model.current_tab != load_tab_action.tab_name);
+                REQUIRE (store->current_tab != load_tab_action.tab_name);
             }
             THEN ("a load tab effect is created")
             {
