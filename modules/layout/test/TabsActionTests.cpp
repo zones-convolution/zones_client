744a388c324d88f760521bf60c9e4112c22e885a
LeonPS <leonps531@gmail.com>
2023-08-01 11:40:24 +0100

    Testing lager effects

--- modules/layout/test/TabsActionTests.cpp
+++ modules/layout/test/TabsActionTests.cpp
@@ -7,22 +7,9 @@
 #include <lager/context.hpp>
 #include <lager/event_loop/manual.hpp>
 #include <lager/store.hpp>
+
 // lager::detail::dispatcher<lager::as_actions_t<TabsAction>> dispatcher {
 //    [] (auto && act) { return lager::promise::invalid (); }};
-
-//        lager::invoke_reducer<TabsEffect::deps_t, decltype (UpdateTabs), TabsModel,
-//        TabsAction> (
-//            UpdateTabs,
-//            TabsModel {},
-//            LoadTabAction {},
-//            [&] (TabsEffect & effect)
-//            {
-//                auto deps = lager::with_deps (
-//                    std::reference_wrapper<TabsControllerDelegate>
-//                    (tabs_controller_delegate_mock));
-//                effect (lager::make_deps (deps));
-//            },
-//            [&] { jassert (false); });
 
 struct TabsControllerDelegateMock : public TabsControllerDelegate
 {
@@ -44,37 +31,24 @@ SCENARIO ("updating tabs model", "[TabsAction]")
     {
         TabsControllerDelegateMock tabs_controller_delegate_mock;
 
-        auto store = lager::make_store<TabsAction> (
-            TabsModel {},
-            WithJuceEventLoop {},
-            lager::with_reducer (&UpdateTabs),
-            lager::with_deps (
-                std::reference_wrapper<TabsControllerDelegate> (tabs_controller_delegate_mock)));
-
-        WHEN ("a load tab action is dispatched")
-        {
-            tabs_controller_delegate_mock.should_return_success = false;
-            LoadTabAction load_tab_action {.tab_name = "test_tab"};
-            store.dispatch (load_tab_action);
-
-            THEN ("the current tab has not changed")
+        auto deps = lager::deps<TabsControllerDelegate &>::with (
+            std::reference_wrapper<TabsControllerDelegate> (tabs_controller_delegate_mock));
+        auto loop = lager::with_manual_event_loop {};
+        lager::context<TabsAction, lager::deps<TabsControllerDelegate &>> context (
+            [] (auto && act) -> lager::future
             {
-                REQUIRE (tabs_controller_delegate_mock.last_tab_name == load_tab_action.tab_name);
-                REQUIRE (store->current_tab != load_tab_action.tab_name);
-            }
-        }
+                jassert (false);
+                return lager::future ();
+            },
+            loop,
+            deps);
 
         WHEN ("a load tab action is dispatched")
         {
             tabs_controller_delegate_mock.should_return_success = true;
             LoadTabAction load_tab_action {.tab_name = "test_tab"};
-            store.dispatch (load_tab_action);
-
-            THEN ("the current tab has updated")
-            {
-                REQUIRE (tabs_controller_delegate_mock.last_tab_name == load_tab_action.tab_name);
-                REQUIRE (store->current_tab == load_tab_action.tab_name);
-            }
+            auto [model, effect] = UpdateTabs (TabsModel {}, load_tab_action);
+            effect (context);
         }
     }
 }
