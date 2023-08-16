a651266f42264fa8a2d971973b2bb2d71697ba6e
LeonPS <leonps531@gmail.com>
2023-08-16 13:53:13 +0100

    Allow icons to have colour set

--- modules/zones_look_and_feel/src/zones_look_and_feel/components/IconComponent.cpp
+++ modules/zones_look_and_feel/src/zones_look_and_feel/components/IconComponent.cpp
@@ -2,12 +2,17 @@
 
 void IconComponent::paint (juce::Graphics & g)
 {
-    auto label_colour = findColour (juce::Label::ColourIds::textColourId);
-    g.setColour (label_colour);
+    g.setColour (colour_.has_value () ? colour_.value ()
+                                      : findColour (juce::Label::ColourIds::textColourId));
     LookAndFeel::DrawBoxIcon (g, icon_, getLocalBounds ());
 }
 
 void IconComponent::SetIcon (const juce::String & icon)
 {
     icon_ = icon;
+}
+
+void IconComponent::SetColour (const juce::Colour & colour)
+{
+    colour_ = colour;
 }
