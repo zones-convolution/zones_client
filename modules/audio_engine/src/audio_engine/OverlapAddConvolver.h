623f3be7cdd4e2a777fb5b65000a7c81267b0047
Micah Strange <micasans@btinternet.com>
2023-11-04 18:40:37 +0000

    overlap add convolver

---
+++ modules/audio_engine/src/audio_engine/OverlapAddConvolver.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <juce_dsp/juce_dsp.h>
+
+class OverlapAddConvolver : public juce::dsp::ProcessorBase
+{
+public:
+    void prepare (const juce::dsp::ProcessSpec & spec) override;
+    void process (const juce::dsp::ProcessContextReplacing<float> & replacing) override;
+    void reset () override;
+    void LoadImpulseResponse (juce::dsp::AudioBlock<float> ir_block, double sample_rate);
+
+private:
+    int read_position_ = 0;
+    int fft_size_ = 0;
+    juce::AudioBuffer<float> delay_line_;
+    juce::dsp::ProcessSpec process_spec_;
+
+    std::unique_ptr<juce::dsp::FFT> fft_;
+    juce::AudioBuffer<float> ir_forward_transform_;
+    juce::AudioBuffer<float> input_block_forward_transform_;
+};
