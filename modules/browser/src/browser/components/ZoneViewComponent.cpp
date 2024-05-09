bb1d2e827093cd5be33e34bf1b97cec53aa83b19
LeonPS <leon@leonps.com>
2024-05-09 12:30:12 +0100

    Select specific ir in zone

--- modules/browser/src/browser/components/ZoneViewComponent.cpp
+++ modules/browser/src/browser/components/ZoneViewComponent.cpp
@@ -1,17 +1,52 @@
 #include "ZoneViewComponent.h"
 
-ZoneViewComponent::ZoneViewComponent ()
+#include "look_and_feel/LookAndFeel.h"
+
+ZoneViewComponent::ZoneViewComponent (lager::context<Action> & context)
+    : context_ (context)
 {
     addAndMakeVisible (label_);
+
+    addAndMakeVisible (ir_selection_);
+
+    load_.onClick = [&]
+    {
+        auto & irs = zone_metadata_.irs;
+        auto selected_id = ir_selection_.getSelectedItemIndex ();
+        auto selected_ir = irs [selected_id];
+
+        context.dispatch (LoadIrAction {.ir_selection = {.zone = zone_metadata_,
+                                                         .ir = selected_ir,
+                                                         .target_format = TargetFormat::kStereo}});
+    };
+
+    addAndMakeVisible (load_);
 }
 
 void ZoneViewComponent::resized ()
 {
-    label_.setBounds (getLocalBounds ());
+    juce::FlexBox layout;
+    layout.flexDirection = juce::FlexBox::Direction::column;
+
+    layout.items.add (LookAndFeel::LabelFlexItem (label_));
+    layout.items.add (LookAndFeel::kFlexSpacer);
+    layout.items.add (LookAndFeel::ComboFlexItem (ir_selection_));
+    layout.items.add (LookAndFeel::kFlexSpacer);
+    layout.items.add (LookAndFeel::ButtonFlexItem (load_));
+
+    layout.performLayout (getLocalBounds ());
 }
 
 void ZoneViewComponent::Update (const ZoneView & zone_view)
 {
+    zone_metadata_ = zone_view.zone_metadata;
     auto & zone_metadata = zone_view.zone_metadata;
     label_.setText (zone_metadata.title, juce::dontSendNotification);
+
+    ir_selection_.clear (juce::dontSendNotification);
+    auto & irs = zone_metadata.irs;
+    for (auto i = 0; i < irs.size (); ++i)
+        ir_selection_.addItem (*irs [i].title, i + 1);
+
+    ir_selection_.setSelectedItemIndex (0);
 }
--- modules/browser/src/browser/components/ZoneViewComponent.h
+++ modules/browser/src/browser/components/ZoneViewComponent.h
@@ -1,16 +1,24 @@
 #pragma once
 
 #include "../BrowserModel.h"
+#include "look_and_feel/BoxIcons.h"
+#include "look_and_feel/components/IconTextButton.h"
+#include "model/Action.h"
 
 #include <juce_gui_basics/juce_gui_basics.h>
 
 class ZoneViewComponent : public juce::Component
 {
 public:
-    ZoneViewComponent ();
+    ZoneViewComponent (lager::context<Action> & context);
     void Update (const ZoneView & zone_view);
     void resized () override;
 
 private:
+    lager::context<Action> context_;
+
+    ZoneMetadata zone_metadata_;
+    IconTextButton load_ {"Load Zone", BoxIcons::kBxPlay};
+    juce::ComboBox ir_selection_;
     juce::Label label_ {"Zone View", "Zone View"};
 };
