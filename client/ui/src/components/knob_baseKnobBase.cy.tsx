281a00afd3c9f0c584a8502de2a41cfa217c2377
LeonPS <leon@leonps.com>
2024-07-02 12:50:18 +0100

    Building in release

--- client/ui/src/components/knob_baseKnobBase.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { KnobBase } from './knob_base'
-
-describe('<KnobBase />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<KnobBase />)
-  })
-})
