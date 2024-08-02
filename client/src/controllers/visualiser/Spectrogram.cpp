72b86ac40d5075ffa175821a599ffc6eb15b892e
LeonPS <leon@leonps.com>
2024-08-02 16:50:06 +0100

    Small spec cleanup

--- client/src/controllers/visualiser/Spectrogram.cpp
+++ client/src/controllers/visualiser/Spectrogram.cpp
@@ -4,17 +4,26 @@ static constexpr float kMaxIrRatio = 2.0f;
 static constexpr float kResolution = 1024.0f;
 static constexpr float kReferenceSampleRate = 48000.0f;
 static constexpr int kTargetFFTSize = 1024;
+static constexpr int kTargetNumNonNegativeFFTPoints = kTargetFFTSize / 2;
+
+static int GetFFTSize (int hop_size, double base_sample_rate)
+{
+    auto samples_per_hop = static_cast<int> (std::round (hop_size / kResolution));
+    return std::max (static_cast<int> (juce::nextPowerOfTwo (2 * samples_per_hop)), kTargetFFTSize);
+}
+
+static int GetHopSize (int base_num_samples)
+{
+    auto max_num_samples_frac = kMaxIrRatio * base_num_samples;
+    return static_cast<int> (std::round (max_num_samples_frac / kResolution));
+}
 
 static juce::AudioBuffer<float> PerformFFT (const juce::dsp::AudioBlock<const float> & audio_block,
                                             int base_num_samples,
                                             double base_sample_rate)
 {
-    auto max_num_samples_frac = kMaxIrRatio * base_num_samples;
-    auto sample_rate_scale = kReferenceSampleRate / base_sample_rate;
-    auto samples_per_hop = static_cast<int> (std::round ((max_num_samples_frac / kResolution)));
-
-    auto fft_size = static_cast<int> (juce::nextPowerOfTwo (2 * samples_per_hop));
-    fft_size = std::max (fft_size, kTargetFFTSize);
+    auto hop_size = GetHopSize (base_num_samples);
+    auto fft_size = GetFFTSize (hop_size, base_sample_rate);
     auto fft_order = static_cast<int> (std::log2 (fft_size));
 
     juce::AudioBuffer<float> fft_data {1, fft_size * 2};
@@ -25,7 +34,7 @@ static juce::AudioBuffer<float> PerformFFT (const juce::dsp::AudioBlock<const fl
 
     auto num_samples = static_cast<int> (audio_block.getNumSamples ());
     auto num_hops = static_cast<int> (
-        std::ceil (static_cast<float> (num_samples) / static_cast<float> (samples_per_hop)));
+        std::ceil (static_cast<float> (num_samples) / static_cast<float> (hop_size)));
 
     juce::AudioBuffer<float> frequency_data {
         num_hops, (fft_size / 2)}; // Should actually have a + 1 here I think
@@ -34,7 +43,7 @@ static juce::AudioBuffer<float> PerformFFT (const juce::dsp::AudioBlock<const fl
 
     for (auto hop_index = 0; hop_index < num_hops; ++hop_index)
     {
-        auto sample_index = hop_index * samples_per_hop;
+        auto sample_index = hop_index * hop_size;
         auto block_size = std::min (fft_size, num_samples - sample_index);
         auto sub_block = audio_block.getSubBlock (sample_index, block_size);
 
@@ -142,7 +151,7 @@ AverageFrequencyData (const juce::dsp::AudioBlock<const float> & frequency_data)
         interpolator.process (ratio,
                               frequency_data.getChannelPointer (channel_index),
                               averaged_frequency_data.getWritePointer (channel_index),
-                              kTargetFFTSize / 2);
+                              kTargetNumNonNegativeFFTPoints);
     }
 
     return averaged_frequency_data;
@@ -155,9 +164,7 @@ Spectrogram::CreateNormalisedSpectrogramData (Spectrogram::BoxedBuffer buffer,
 {
     auto frequency_data = PerformFFT (*buffer, base_num_sample, base_sample_rate);
 
-    if (frequency_data.getNumSamples () >
-        (kTargetFFTSize /
-         2)) // Divied by 2 here because frquency only with no negative is half size...
+    if (frequency_data.getNumSamples () > kTargetNumNonNegativeFFTPoints)
         frequency_data = AverageFrequencyData (frequency_data);
     NormaliseFrequencyData (frequency_data);
 
