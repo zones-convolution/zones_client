407bfd648a888f2c184b121f3a7ffc9253da02d3
Micah Strange <micahstrange16@gmail.com>
2025-04-21 12:25:55 +0100

    user name on zone page

--- client/ui/src/pages/zone.tsx
+++ client/ui/src/pages/zone.tsx
@@ -3,17 +3,24 @@ import { FC } from "react";
 import { Separator } from "@/components/ui/separator";
 
 import { BannerImageGallery } from "@/components/banner_image_gallery";
+import { UserProfile } from "@/components/user_profile";
 import { IrTable } from "@/components/zone_page/ir_table";
-import { getImageUrl } from "@/lib/s3_resources";
+import { getImageUrl, getProfileImageUrl } from "@/lib/s3_resources";
 import { toZoneMetadata, IZone } from "@/lib/zones";
 
 const Zone: FC<{ zone: IZone }> = ({ zone }) => {
   const zoneMetadata = toZoneMetadata(zone);
+  const profileImageUrl = getProfileImageUrl(zone.user.id);
 
   return (
     <div className="h-full overflow-scroll bg-card">
       <div className="flex flex-col gap-4 h-full p-4">
-        <h1 className="text-xl">{zone.title}</h1>
+        <div className="flex flex-row justify-between">
+          <h1 className="text-xl">{zone.title}</h1>
+          {zone.user.name && (
+            <UserProfile name={zone.user.name} imageUrl={profileImageUrl} />
+          )}
+        </div>
         <div className="h-[300px]">
           <BannerImageGallery
             imageUrls={zone.images.map((image) =>
